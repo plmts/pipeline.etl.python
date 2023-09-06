a738c7a6bb416caedde06c19bd608258d9745524
Paulo Matos <90912163+plmts@users.noreply.github.com>
2023-09-06 14:53:51 -0300

    Algumas alterações foram feitas. Percebi que não precisava de duas funções. Projeto finalizado.

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 import pandas as pd
+from sqlalchemy import create_engine
 
 #extração
-def extrair_dados():
-    """função para extrair os dados do bd"""
-    df = pd.read_csv('./dados.csv')
-    dados_filtrados = df['OwnerName']
-    return dados_filtrados
+df = pd.read_csv('./dados.csv')
 
 #transformação
 def carregar_dados(dados_filtrados):
     """Função para transformar os dados filtrados em um novo arquivo"""
-    arquivo_filtrado = dados_filtrados.to_csv("dados_filtrados.csv", index=False)
+    arquivo_filtrado = df['OwnerName']
     return arquivo_filtrado
 
+#Carregamento
+novos_dados = carregar_dados(df)
+novos_dados.to_csv("dados_filtrados.csv", index=False)
